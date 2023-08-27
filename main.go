67ffddfcae4fb5af5bb134701f30bf8a355e6316
Wollaston <mattflynn1997@gmail.com>
2023-08-27 17:37:49 -0400

    Incorporate tailwind and static serving of css

--- main.go
+++ main.go
@@ -8,21 +8,26 @@ import (
 )
 
 type Todo struct {
-	Title	string
-	Detail	string
+	Title  string
+	Detail string
 }
 
 func main() {
 	http.HandleFunc("/", DefaultHandler)
+	http.HandleFunc("/static/output.css", ServeStyleSheet)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func DefaultHandler(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("./public/index.html"))
-		tmpl.Execute(w, nil)
-	}
+	tmpl := template.Must(template.ParseFiles("./src/public/index.html"))
+	tmpl.Execute(w, nil)
+}
 
 func AddItemHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("AddItemHandler")
+}
+
+func ServeStyleSheet(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/output.css")
 }
