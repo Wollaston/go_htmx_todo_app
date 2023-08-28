4917a7a1efccf01719324a6a93785edb8fd59e39
Wollaston <mattflynn1997@gmail.com>
2023-08-27 22:20:43 -0400

    Refactor db functions into own file

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"html/template"
 	"log"
 	"net/http"
-	"time"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 type Todo struct {
@@ -45,57 +41,6 @@ func ToDoHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "todo-list-element", Todo{Title: r.FormValue("title"), Detail: r.FormValue("detail")})
 
 	Close(db)
-}
-
-func Connect() *sql.DB {
-	db, err := sql.Open("sqlite3", "./todos.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Connected to DB")
-
-	return db
-}
-
-func Read(db *sql.DB) []Todo {
-	rows, err := db.Query("SELECT * FROM todos")
-	if err != nil {
-		log.Fatal(err)
-	}
-	var records []Todo
-
-	for rows.Next() {
-		var uid int
-		var title string
-		var detail string
-		var created time.Time
-		err = rows.Scan(&uid, &title, &detail, &created)
-		if err != nil {
-			log.Fatal(err)
-		}
-		record := Todo{Title: title, Detail: detail}
-		records = append(records, record)
-	}
-	return records
-}
-
-func Create(db *sql.DB, title string, detail string) {
-	stmt, err := db.Prepare("INSERT INTO todos(title, detail, created) values(?,?,?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	res, err := stmt.Exec(title, detail, time.Now())
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("DB Create: ")
-	fmt.Println(res)
-}
-
-func Close(db *sql.DB) {
-	db.Close()
-	fmt.Println("DB Connection Closed.")
 }
 
 func CreateTodos(w http.ResponseWriter, r *http.Request) {
