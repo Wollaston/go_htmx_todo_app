914cdb9dc88db4711e0cb9b389ea4587dbde87c5
Wollaston <mattflynn1997@gmail.com>
2023-08-27 20:46:36 -0400

    Post form submission to sqlite db

--- main.go
+++ main.go
@@ -20,11 +20,7 @@ func main() {
 	http.HandleFunc("/", DefaultHandler)
 	http.HandleFunc("/static/output.css", ServeStyleSheet)
 	http.HandleFunc("/clicked", ClickHandler)
-
-	db := Connect()
-	Create(db, "Test1", "Test2")
-	Read(db)
-	Close(db)
+	http.HandleFunc("/create-todo", ToDoHandler)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
@@ -47,7 +43,16 @@ func ClickHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("ClickHandler")
 }
 
-func Connect() (*sql.DB) {
+func ToDoHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	db := Connect()
+	Create(db, r.FormValue("title"), r.FormValue("detail"))
+	Read(db)
+	Close(db)
+}
+
+func Connect() *sql.DB {
 	db, err := sql.Open("sqlite3", "./todos.db")
 	if err != nil {
 		log.Fatal(err)
@@ -64,23 +69,36 @@ func Read(db *sql.DB) {
 		log.Fatal(err)
 	}
 	fmt.Println("DB Read: ")
-	fmt.Println(rows)
+	for rows.Next() {
+		var uid int
+		var title string
+		var detail string
+		var created time.Time
+		err = rows.Scan(&uid, &title, &detail, &created)
+		if err != nil {
+		 log.Fatal(err)
+		}
+		fmt.Println(uid)
+		fmt.Println(title)
+		fmt.Println(detail)
+		fmt.Println(created)
+	 }
 }
 
 func Create(db *sql.DB, title string, detail string) {
 	stmt, err := db.Prepare("INSERT INTO todos(title, detail, created) values(?,?,?)")
 	if err != nil {
 		log.Fatal(err)
-	 }
+	}
 	res, err := stmt.Exec(title, detail, time.Now())
 	if err != nil {
 		log.Fatal(err)
-	 }
+	}
 	fmt.Println("DB Create: ")
 	fmt.Println(res)
 }
 
-func Close(db *sql.DB){
+func Close(db *sql.DB) {
 	db.Close()
 	fmt.Println("DB Connection Closed.")
 }
